9db299d3acf8205956f77be609e07d0715412bd2
병우 유 <conner903@naver.com>
2021-06-10 00:38:26 +0900

    Update: Server

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,5 @@
-const http = require('http');
+var server = require('./server');
+var router = require('./router');
+var requestHandlers = require('./requestHandler');
 
-http.createServer((request,response) => {
-    return request
-    .on('error',(err) => {console.log(err);})
-    .on('data',(data) => {console.log(data);})
-    .on('end', () => {
-        response.on('error', (err)=>{
-            console.error(err);
-        });
-        response.statusCode = 200;
-        response.setHeader('Content-Type','text/plain');
-        response.write('hi\n');
-        response.end('the end!');
-    });
-}).listen(8080);
+server.start(router.route, requestHandlers.handle);
